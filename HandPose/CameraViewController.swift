91ddc2e0e6a36967bbef6dc0e96f0967bacdab6e
AlohaYos <yos@newtonjapan.com>
2023-07-12 17:54:10 +0900

    ジェスチャー部分のコーディングをシンプルにしたSpatialGestureProcessorBaseクラスを追加

--- HandPose/CameraViewController.swift
+++ HandPose/CameraViewController.swift
@@ -12,6 +12,9 @@ import Vision
 // カメラ画面
 class CameraViewController: UIViewController {
 
+	private let useSpatialGesture = true
+	private let devicePosition: AVCaptureDevice.Position = .front	// .front セルフィー / .back 裏面カメラ
+	
 	// カメラに写った画面を表示するビュー。この上に曲線を表示していく（詳細はCameraView.swiftを参照）
     private var cameraView: CameraView { view as! CameraView }
     
@@ -30,6 +33,9 @@ class CameraViewController: UIViewController {
 	private var gestureProcessor = HandGestureProcessor()				// 2本指ジェスチャーを判定するクラス
 	private var handPoseRequest = VNDetectHumanHandPoseRequest()		// 指の関節の位置情報を検出するためのクラス
 
+	// 空間ジェスチャー
+	private var spatialGestureProcessor = SpatialGestureProcessorBase()		// 空間ジェスチャーを判定するクラス
+
 	// アプリ画面が表示される直前の処理
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,7 +49,12 @@ class CameraViewController: UIViewController {
         view.layer.addSublayer(drawOverlay)
 
 		// 手のポーズを検出するのは片手だけ
-        handPoseRequest.maximumHandCount = 1
+		if(useSpatialGesture) {
+			handPoseRequest.maximumHandCount = 2
+		}
+		else {
+			handPoseRequest.maximumHandCount = 1
+		}
 		// ジェスチャーが検知された時に行う処理handleGestureStateChangeを登録する
         gestureProcessor.didChangeStateClosure = { [weak self] state in
             self?.handleGestureStateChange(state: state)
@@ -84,10 +95,7 @@ class CameraViewController: UIViewController {
     }
     
     func setupAVSession() throws {
-        // Select a front facing camera, make an input.
-		// どちら側のカメラかを指定してビデオデバイスを生成する
-//		guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {	// 裏面カメラ
-		guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {	// セルフィーカメラ
+		guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: devicePosition) else {
             throw AppError.captureSessionSetup(reason: "Could not find a front facing camera.")
         }
         // ビデオ入力を生成する
@@ -122,110 +130,6 @@ class CameraViewController: UIViewController {
         cameraFeedSession = session
 }
     
-	// 親指と人差し指の先端ポイントを処理する
-    func processPoints(thumbTip: CGPoint?, indexTip: CGPoint?) {
-        // Check that we have both points.
-		// 親指と人差し指の両方のポインタが検出できていない場合は
-        guard let thumbPoint = thumbTip, let indexPoint = indexTip else {
-			// 親指と人差し指の両方が2秒以上検出できなかったら、いったんジェスチャー検出をリセットして再開
-            if Date().timeIntervalSince(lastObservationTimestamp) > 2 {
-                gestureProcessor.reset()
-            }
-			// 曲線をクリア
-            cameraView.showPoints([], color: .clear)
-            return
-        }
-        
-		// カメラ座標系（AVFoundation coordinates）から画面座標系（UIKit coordinates）に変換
-        let previewLayer = cameraView.previewLayer
-        let thumbPointConverted = previewLayer.layerPointConverted(fromCaptureDevicePoint: thumbPoint)
-        let indexPointConverted = previewLayer.layerPointConverted(fromCaptureDevicePoint: indexPoint)
-        
-		// 2点の位置関係を判断して、ジェスチャー状況を判断する
-        gestureProcessor.processPointsPair((thumbPointConverted, indexPointConverted))
-    }
-    
-	// ジェスチャーが検知された時に行う処理
-    private func handleGestureStateChange(state: HandGestureProcessor.State) {
-        let pointsPair = gestureProcessor.lastProcessedPointsPair	// 直前の親指と人差し指の位置
-        var tipsColor: UIColor
-        switch state {
-        case .possiblePinch, .possibleApart:
-			// まだ指がくっついたとも離れたとも判断できていない状態なので曲線を描画できない。
-			// 今後、状況によっては描画することになるため、指の座標をevidenceBufferに保存しておく
-            evidenceBuffer.append(pointsPair)
-			// 指先を示す点の色はオレンジ
-            tipsColor = .orange
-        case .pinched:
-			// 指がくっついたと確証が持てた場合の処理
-			// evidenceBuffer内の指の座標を使って曲線を生成する
-            for bufferedPoints in evidenceBuffer {
-                updatePath(with: bufferedPoints, isLastPointsPair: false)
-            }
-			// evidenceBufferに保存してあった情報を消す
-            evidenceBuffer.removeAll()
-            // Finally, draw the current point.
-			// 最新の指の座標も曲線に追加
-            updatePath(with: pointsPair, isLastPointsPair: false)	// 曲線描画は継続
-			// 指先を示す点の色はグリーン
-            tipsColor = .green
-        case .apart, .unknown:
-			// 指が離れたと確証が持てた場合の処理
-			// evidenceBuffer内の保存データを削除する
-            evidenceBuffer.removeAll()
-            // And draw the last segment of our draw path.
-			//
-            updatePath(with: pointsPair, isLastPointsPair: true)	// 曲線描画は完了
-			// 指先を示す点の色は赤
-            tipsColor = .red
-        }
-		// カメラ画面上に指先を示す点を表示する
-        cameraView.showPoints([pointsPair.thumbTip, pointsPair.indexTip], color: tipsColor)
-    }
-    
-	// 曲線を作る
-    private func updatePath(with points: HandGestureProcessor.PointsPair, isLastPointsPair: Bool) {
-		// 親指と人差し指の中間点の座標を計算
-        let (thumbTip, indexTip) = points
-        let drawPoint = CGPoint.midPoint(p1: thumbTip, p2: indexTip)	// 中間点
-
-		// 曲線の最後の点か？
-        if isLastPointsPair {
-            if let lastPoint = lastDrawPoint {
-				// 最後に描画した点まで線を引く
-                drawPath.addLine(to: lastPoint)
-            }
-			// 曲線の描画は終了。最後に描画した点もリセット
-            lastDrawPoint = nil
-		// 曲線の描画途中
-        } else {
-			// 曲線の描きはじめ？
-            if lastDrawPoint == nil {
-				// 開始点まで移動する
-                drawPath.move(to: drawPoint)
-                isFirstSegment = true
-			// すでに曲線を作成中
-            } else {
-                let lastPoint = lastDrawPoint!
-				// 今の指座標と直前の点との中間を計算する
-                let midPoint = CGPoint.midPoint(p1: lastPoint, p2: drawPoint)
-                if isFirstSegment {
-                    // If it's the first segment of the stroke, draw a line to the midpoint.
-					// 描画ストロークの最初のセグメントであれば、直線を引く
-                    drawPath.addLine(to: midPoint)
-                    isFirstSegment = false
-                } else {
-					// 描画ストロークの最初でなければ、最後の点をコントロールポイントとして中間点までカーブを描く
-                    drawPath.addQuadCurve(to: midPoint, controlPoint: lastPoint)
-                }
-            }
-			// 次回のために最後に描画した点を記憶しておく
-            lastDrawPoint = drawPoint
-        }
-		// 曲線を描画するレイヤーに渡す
-        drawOverlay.path = drawPath.cgPath
-    }
-
 	// 画面をダブルタップされた時の処理
     @IBAction func handleGesture(_ gesture: UITapGestureRecognizer) {
 		// ジェスチャーが完了するまでは何もしない
@@ -236,59 +140,197 @@ class CameraViewController: UIViewController {
         drawPath.removeAllPoints()			// 曲線描画用データをクリア
         drawOverlay.path = drawPath.cgPath	// クリアされた情報を曲線描画するレイヤーに渡す（曲線をクリアするために）
     }
+	
+	// MARK: Sample Gesture
+	// 親指と人差し指の先端ポイントを処理する
+	func processPoints(thumbTip: CGPoint?, indexTip: CGPoint?) {
+		// Check that we have both points.
+		// 親指と人差し指の両方のポインタが検出できていない場合は
+		guard let thumbPoint = thumbTip, let indexPoint = indexTip else {
+			// 親指と人差し指の両方が2秒以上検出できなかったら、いったんジェスチャー検出をリセットして再開
+			if Date().timeIntervalSince(lastObservationTimestamp) > 2 {
+				gestureProcessor.reset()
+			}
+			// 曲線をクリア
+			cameraView.showPoints([], color: .clear)
+			return
+		}
+		
+		// カメラ座標系（AVFoundation coordinates）から画面座標系（UIKit coordinates）に変換
+		let previewLayer = cameraView.previewLayer
+		let thumbPointConverted = previewLayer.layerPointConverted(fromCaptureDevicePoint: thumbPoint)
+		let indexPointConverted = previewLayer.layerPointConverted(fromCaptureDevicePoint: indexPoint)
+		
+		// 2点の位置関係を判断して、ジェスチャー状況を判断する
+		gestureProcessor.processPointsPair((thumbPointConverted, indexPointConverted))
+	}
+	
+	// ジェスチャーが検知された時に行う処理
+	private func handleGestureStateChange(state: HandGestureProcessor.State) {
+		let pointsPair = gestureProcessor.lastProcessedPointsPair	// 直前の親指と人差し指の位置
+		var tipsColor: UIColor
+		switch state {
+		case .possiblePinch, .possibleApart:
+			// まだ指がくっついたとも離れたとも判断できていない状態なので曲線を描画できない。
+			// 今後、状況によっては描画することになるため、指の座標をevidenceBufferに保存しておく
+			evidenceBuffer.append(pointsPair)
+			// 指先を示す点の色はオレンジ
+			tipsColor = .orange
+		case .pinched:
+			// 指がくっついたと確証が持てた場合の処理
+			// evidenceBuffer内の指の座標を使って曲線を生成する
+			for bufferedPoints in evidenceBuffer {
+				updatePath(with: bufferedPoints, isLastPointsPair: false)
+			}
+			// evidenceBufferに保存してあった情報を消す
+			evidenceBuffer.removeAll()
+			// Finally, draw the current point.
+			// 最新の指の座標も曲線に追加
+			updatePath(with: pointsPair, isLastPointsPair: false)	// 曲線描画は継続
+			// 指先を示す点の色はグリーン
+			tipsColor = .green
+		case .apart, .unknown:
+			// 指が離れたと確証が持てた場合の処理
+			// evidenceBuffer内の保存データを削除する
+			evidenceBuffer.removeAll()
+			// And draw the last segment of our draw path.
+			//
+			updatePath(with: pointsPair, isLastPointsPair: true)	// 曲線描画は完了
+			// 指先を示す点の色は赤
+			tipsColor = .red
+		}
+		// カメラ画面上に指先を示す点を表示する
+		cameraView.showPoints([pointsPair.thumbTip, pointsPair.indexTip], color: tipsColor)
+	}
+	
+	// 曲線を作る
+	private func updatePath(with points: HandGestureProcessor.PointsPair, isLastPointsPair: Bool) {
+		// 親指と人差し指の中間点の座標を計算
+		let (thumbTip, indexTip) = points
+		let drawPoint = CGPoint.midPoint(p1: thumbTip, p2: indexTip)	// 中間点
+
+		// 曲線の最後の点か？
+		if isLastPointsPair {
+			if let lastPoint = lastDrawPoint {
+				// 最後に描画した点まで線を引く
+				drawPath.addLine(to: lastPoint)
+			}
+			// 曲線の描画は終了。最後に描画した点もリセット
+			lastDrawPoint = nil
+		// 曲線の描画途中
+		} else {
+			// 曲線の描きはじめ？
+			if lastDrawPoint == nil {
+				// 開始点まで移動する
+				drawPath.move(to: drawPoint)
+				isFirstSegment = true
+			// すでに曲線を作成中
+			} else {
+				let lastPoint = lastDrawPoint!
+				// 今の指座標と直前の点との中間を計算する
+				let midPoint = CGPoint.midPoint(p1: lastPoint, p2: drawPoint)
+				if isFirstSegment {
+					// If it's the first segment of the stroke, draw a line to the midpoint.
+					// 描画ストロークの最初のセグメントであれば、直線を引く
+					drawPath.addLine(to: midPoint)
+					isFirstSegment = false
+				} else {
+					// 描画ストロークの最初でなければ、最後の点をコントロールポイントとして中間点までカーブを描く
+					drawPath.addQuadCurve(to: midPoint, controlPoint: lastPoint)
+				}
+			}
+			// 次回のために最後に描画した点を記憶しておく
+			lastDrawPoint = drawPoint
+		}
+		// 曲線を描画するレイヤーに渡す
+		drawOverlay.path = drawPath.cgPath
+	}
+
 }
+
+// MARK: AVCaptureVideoDataOutputSampleBufferDelegate
 
 // カメラから入った映像が出力バッファに蓄積された時に呼ばれる処理
 extension CameraViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
 	// カメラ映像がキャプチャされた
     public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        var thumbTip: CGPoint?
-        var indexTip: CGPoint?
-        
-		// この関数が終了した後に実行されるブロック
-        defer {
-            DispatchQueue.main.sync {
-				// この関数内で取得した親指thumbTipと人差し指indexTipの座標を処理する
-                self.processPoints(thumbTip: thumbTip, indexTip: indexTip)
-            }
-        }
+		
+		if(useSpatialGesture) {
+			var handPoseObservation: VNHumanHandPoseObservation?
+			defer {
+				DispatchQueue.main.sync {
+					guard let observation = handPoseObservation else {
+						return
+					}
+					spatialGestureProcessor.processHandPoseObservation(observation: handPoseObservation!, cameraView: cameraView)
+				}
+			}
+			let handler = VNImageRequestHandler(cmSampleBuffer: sampleBuffer, orientation: .up, options: [:])
+			do {
+				try handler.perform([handPoseRequest])
+				guard let observation = handPoseRequest.results?.first else { // observation: VNHumanHandPoseObservation
+					handPoseObservation = nil
+					return
+				}
+				handPoseObservation = observation
+			} catch {
+				cameraFeedSession?.stopRunning()
+				let error = AppError.visionError(error: error)
+				DispatchQueue.main.async {
+					error.displayInViewController(self)
+				}
+			}
+		}
+		else {
+			var thumbTip: CGPoint?
+			var indexTip: CGPoint?
+			
+			// この関数が終了した後に実行されるブロック
+			defer {
+				DispatchQueue.main.sync {
+					// この関数内で取得した親指thumbTipと人差し指indexTipの座標を処理する
+					self.processPoints(thumbTip: thumbTip, indexTip: indexTip)
+				}
+			}
 
-		// sampleBufferのカメラ画像を使ってイメージ処理する
-        let handler = VNImageRequestHandler(cmSampleBuffer: sampleBuffer, orientation: .up, options: [:])
-        do {
-			// 手のポーズ検出をリクエスト（トライする）
-            try handler.perform([handPoseRequest])
-			// 手のポーズが検出されなかったらリターン
-			// 手の検出数を1に設定しているので、それ以外の場合はエラーになっているはず
-            guard let observation = handPoseRequest.results?.first else {
-                return
-            }
-			// 親指と人差し指の全ての関節位置を取得する
-            let thumbPoints = try observation.recognizedPoints(.thumb)
-            let indexFingerPoints = try observation.recognizedPoints(.indexFinger)
-			// それぞれの指先位置を取得する
-			// 取得できなかったらリターン
-            guard let thumbTipPoint = thumbPoints[.thumbTip], let indexTipPoint = indexFingerPoints[.indexTip] else {
-                return
-            }
-            // Ignore low confidence points.
-			// 画像判断の結果、確証が低い（指先だと確証が持てない）場合はリターン
-            guard thumbTipPoint.confidence > 0.3 && indexTipPoint.confidence > 0.3 else {
-                return
-            }
-			// 指先座標をVisionKit系（画像認識系）からAVFoundation系（ビデオ系）へ変換
-            thumbTip = CGPoint(x: thumbTipPoint.location.x, y: 1 - thumbTipPoint.location.y)
-            indexTip = CGPoint(x: indexTipPoint.location.x, y: 1 - indexTipPoint.location.y)
-		// doブロックの処理中にエラーが発生した場合
-        } catch {
-			// カメラのセッションを停止（カメラ画像が更新されなくなる）
-            cameraFeedSession?.stopRunning()
-			// エラーを表示する
-            let error = AppError.visionError(error: error)
-            DispatchQueue.main.async {
-                error.displayInViewController(self)
-            }
-        }
+			// sampleBufferのカメラ画像を使ってイメージ処理する
+			let handler = VNImageRequestHandler(cmSampleBuffer: sampleBuffer, orientation: .up, options: [:])
+			do {
+				// 手のポーズ検出をリクエスト（トライする）
+				try handler.perform([handPoseRequest])
+				// 手のポーズが検出されなかったらリターン
+				// 手の検出数を1に設定しているので、それ以外の場合はエラーになっているはず
+				guard let observation = handPoseRequest.results?.first else {
+					return
+				}
+				// 親指と人差し指の全ての関節位置を取得する
+				let thumbPoints = try observation.recognizedPoints(.thumb)
+				let indexFingerPoints = try observation.recognizedPoints(.indexFinger)
+				// それぞれの指先位置を取得する
+				// 取得できなかったらリターン
+				guard let thumbTipPoint = thumbPoints[.thumbTip], let indexTipPoint = indexFingerPoints[.indexTip] else {
+					return
+				}
+				// Ignore low confidence points.
+				// 画像判断の結果、確証が低い（指先だと確証が持てない）場合はリターン
+				guard thumbTipPoint.confidence > 0.3 && indexTipPoint.confidence > 0.3 else {
+					return
+				}
+				// 指先座標をVisionKit系（画像認識系）からAVFoundation系（ビデオ系）へ変換
+				thumbTip = CGPoint(x: thumbTipPoint.location.x, y: 1 - thumbTipPoint.location.y)
+				indexTip = CGPoint(x: indexTipPoint.location.x, y: 1 - indexTipPoint.location.y)
+			// doブロックの処理中にエラーが発生した場合
+			} catch {
+				// カメラのセッションを停止（カメラ画像が更新されなくなる）
+				cameraFeedSession?.stopRunning()
+				// エラーを表示する
+				let error = AppError.visionError(error: error)
+				DispatchQueue.main.async {
+					error.displayInViewController(self)
+				}
+			}
+		}
     }
 }
+
 
---
+++ HandPose/SpatialGestureProcessorBase.swift
@@ -0,0 +1,153 @@
+/*
+	空間ジェスチャーのベースクラス
+*/
+
+import CoreGraphics
+import UIKit
+import Vision
+
+// ハンドジェスチャーを判定するクラス
+class SpatialGestureProcessorBase {
+// MARK: 列挙
+	// 判定状態のバリエーション（いまどんな状況かを表す）
+	enum State {
+		case possiblePinch
+		case pinched
+		case possibleApart
+		case apart
+		case unknown
+	}
+
+// MARK: 変数（プロパティ）
+
+	// 外部のクラスからアクセスされる可能性のある変数/定義
+	typealias PointsPair = (thumbTip: CGPoint, indexTip: CGPoint)	// 2点をまとめて取り扱うための定義
+	var didChangeStateClosure: ((State) -> Void)?	// stateが変化した時に呼び出す関数（上位のクラスCameraViewControllerから設定される関数）
+
+	// このクラス内でのみ使用するprivate変数
+	private var cameraView: CameraView!										// カメラ画像を表示するビュー
+	private var tipsColor: UIColor = .red									// 指先を示す点の色
+	private var gestureEvidenceCounter = 0									// ジェスチャーが安定するまでのカウンター
+	private let evidenceCounterStateTrigger: Int							// ジェスチャーの状態がしばらく続いているかを判断する閾値
+	private var pinchEvidenceCounter = 0									// 2点が近づいたカウンター
+	private var apartEvidenceCounter = 0									// 2点が離れたカウンター
+	private let pinchMaxDistance: CGFloat = 40								// 2点が近づいていると判断する基準となる距離
+	private (set) var lastProcessedPointsPair = PointsPair(.zero, .zero)	// 前回の2点
+	private var state = State.unknown {	// 現在の判定状態を保持する変数
+		didSet {
+			didChangeStateClosure?(state)	// stateが変化したらdidChangeStateClosureを呼び出す
+		}
+	}
+
+// MARK: 関数（メソッド）
+	// クラスの初期化。
+	init(evidenceCounterStateTrigger: Int = 3) {
+		self.evidenceCounterStateTrigger = evidenceCounterStateTrigger
+		
+		// ジェスチャーが検知された時に行う処理handleGestureStateChangeを登録する
+		self.didChangeStateClosure = { [weak self] state in
+			self?.handleGestureStateChange(state)
+		}
+		
+		reset()
+	}
+	
+	// 判定状態のリセット
+	func reset() {
+		state = .unknown			// 状況不明
+		pinchEvidenceCounter = 0	// 2点が近づいたカウンター = 0
+		apartEvidenceCounter = 0	// 2点が離れたカウンター = 0
+	}
+
+	// ジェスチャーが検知された時に行う処理
+	private func handleGestureStateChange(_ state: State) {
+
+		switch state {
+		case .possiblePinch:
+			tipsColor = .orange
+			NSLog("指がくっつきそう")
+			break
+		case .pinched:
+			tipsColor = .green
+			NSLog("指がくっついた")
+			break
+		case .possibleApart:
+			tipsColor = .orange
+			NSLog("指が離れそう")
+			break
+		case .apart:
+			tipsColor = .red
+			NSLog("指が離れた")
+			break
+		case .unknown:
+			tipsColor = .red
+			NSLog("不明")
+			break
+		default:
+			tipsColor = .red
+			NSLog("不明")
+			break
+		}
+	}
+
+	// カメラに写った手を画像処理する関数
+	func processHandPoseObservation(observation: VNHumanHandPoseObservation, cameraView: CameraView) {
+		
+		var thumbTip: CGPoint?
+		var indexTip: CGPoint?
+		self.cameraView = cameraView
+		do {
+			// 親指と人差し指の全ての関節位置を取得する
+			let thumbPoints = try observation.recognizedPoints(.thumb)
+			let indexFingerPoints = try observation.recognizedPoints(.indexFinger)
+			// それぞれの指先位置を取得する
+			guard let thumbTipPoint = thumbPoints[.thumbTip], let indexTipPoint = indexFingerPoints[.indexTip] else {
+				// 取得できなかったらリターン
+				return
+			}
+			// 画像判断の結果、確証が低い（指先だと確証が持てない）場合はリターン
+			guard thumbTipPoint.confidence > 0.3 && indexTipPoint.confidence > 0.3 else {
+				return
+			}
+			// 指先座標をVisionKit系（画像認識系）からAVFoundation系（ビデオ系）へ変換
+			thumbTip = CGPoint(x: thumbTipPoint.location.x, y: 1 - thumbTipPoint.location.y)
+			indexTip = CGPoint(x: indexTipPoint.location.x, y: 1 - indexTipPoint.location.y)
+			
+			// AVFoundation系（ビデオ系）から画面座標系（UIKit coordinates）に変換
+			let previewLayer = cameraView.previewLayer
+			let thumbPointConverted = previewLayer.layerPointConverted(fromCaptureDevicePoint: thumbTip!)
+			let indexPointConverted = previewLayer.layerPointConverted(fromCaptureDevicePoint: indexTip!)
+			var pointsPair: PointsPair = (thumbPointConverted, indexPointConverted)
+
+//			NSLog("%f, %f", thumbPointConverted.x, thumbPointConverted.y)
+			
+			// 直前の2点をセーブしておく（CameraViewControllerクラスで使われている）
+			lastProcessedPointsPair = pointsPair
+			
+			// 指先を画面にドット表示
+			self.cameraView.showPoints([pointsPair.thumbTip, pointsPair.indexTip], color: tipsColor)
+
+			// 今回の2点（pointsPair）の距離を計算
+			let distance = pointsPair.indexTip.distance(from: pointsPair.thumbTip)
+			
+			// 判断基準となる2点間距離より近づいていたら
+			if distance < pinchMaxDistance {
+				// その状態がしばらく続くのを観察する（その状態が何回続いたかをカウント）
+				pinchEvidenceCounter += 1	// 近づいた状態をカウントアップ
+				apartEvidenceCounter = 0	// 遠ざかった状態はカウントしない
+				// 数回続いたら pinched、続きそうなら possiblePinch をstateに入れる
+				state = (pinchEvidenceCounter >= evidenceCounterStateTrigger) ? .pinched : .possiblePinch
+				
+				// 判断基準となる2点間距離より離れていたら
+			} else {
+				// その状態がしばらく続くのを観察する（その状態が何回続いたかをカウント）
+				apartEvidenceCounter += 1	// 離れた状態をカウントアップ
+				pinchEvidenceCounter = 0	// 近づいた状態はカウントしない
+				// 数回続いたら apart、続きそうなら possibleApart をstateに入れる
+				state = (apartEvidenceCounter >= evidenceCounterStateTrigger) ? .apart : .possibleApart
+			}
+		} catch {
+			NSLog("エラー発生")
+		}
+	}
+}
